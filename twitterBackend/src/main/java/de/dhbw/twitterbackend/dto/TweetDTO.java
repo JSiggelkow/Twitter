05240928a65cafebbab9319c554640618efecfac
JSiggelkow <J.Siggelkow@iks-gmbh.com>
2025-02-16 13:32:44 +0100

    fixed tweetDTO serializable error when hibernate is proxy loading the user and retweet

--- twitterBackend/src/main/java/de/dhbw/twitterbackend/dto/TweetDTO.java
+++ twitterBackend/src/main/java/de/dhbw/twitterbackend/dto/TweetDTO.java
@@ -1,10 +1,7 @@
 package de.dhbw.twitterbackend.dto;
 
-import de.dhbw.twitterbackend.model.Tweet;
-import de.dhbw.twitterbackend.model.User;
-
 import java.time.OffsetDateTime;
 
-public record TweetDTO(Long id, String text, String video, String image, User userId, Tweet retweetID,
+public record TweetDTO(Long id, String text, String video, String image, String username, Long retweetID,
                        OffsetDateTime createdAt, Long countLikes, Long countRetweets, Long countComments) {
 }
