be97f7b9709ae73572decf6851c5ed3bf3678184
JSiggelkow <J.Siggelkow@iks-gmbh.com>
2025-02-17 15:22:13 +0100

    add tailwind and tailwind primeng plugin

--- twitterFrontend/src/app/app.component.ts
+++ twitterFrontend/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import {Button} from 'primeng/button';
+import {PrimeNG} from 'primeng/config';
+import {MyPreset} from './mytheme';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +11,17 @@ import {Button} from 'primeng/button';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-}
+
+  constructor(private readonly primeng: PrimeNG) {
+        this.primeng.theme.set({
+            preset: MyPreset,
+                options: {
+                    cssLayer: {
+                        name: 'primeng',
+                        order: 'tailwind-base, primeng, tailwind-utilities'
+                    }
+                }
+            })
+        }
+    }
+
