e15b58176bc58128aa5b6d070c2638636979c0d6
JSiggelkow <J.Siggelkow@iks-gmbh.com>
2025-02-18 18:22:35 +0100

    add home and login components

---
+++ twitterFrontend/src/app/components/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {MenuComponent} from './menu/menu.component';
+
+@Component({
+  selector: 'app-home',
+  imports: [
+    MenuComponent
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+  standalone: true
+})
+export class HomeComponent {
+
+}
