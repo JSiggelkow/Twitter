bd43946eafe3f91bc84549a4fc6b10872ee16fd5
JSiggelkow <J.Siggelkow@iks-gmbh.com>
2025-02-19 09:20:35 +0100

    add simple sign up component

--- twitterFrontend/src/app/app.routes.ts
+++ twitterFrontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
 import {LoginComponent} from './components/login/login.component';
 import {authGuard} from './guards/auth.guard';
+import {SignupComponent} from './components/signup/signup.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, canActivate: [authGuard] },
   { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent},
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
---
+++ twitterFrontend/src/app/components/signup/signup.component.ts
@@ -0,0 +1,57 @@
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
+import {AuthService} from '../../service/auth.service';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {InputText} from 'primeng/inputtext';
+import {FloatLabel} from 'primeng/floatlabel';
+import {Password} from 'primeng/password';
+import {Button} from 'primeng/button';
+
+@Component({
+  selector: 'app-signup',
+  imports: [
+    ReactiveFormsModule,
+    InputText,
+    FloatLabel,
+    Password,
+    Button
+  ],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.scss',
+  standalone: true
+})
+export class SignupComponent {
+
+  constructor(
+    private readonly router: Router,
+    private readonly auth: AuthService) {
+  }
+
+  usernameFormControl = new FormControl('', [
+    Validators.required,
+  ]);
+
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ])
+
+  passwordFormControl = new FormControl('', [
+    Validators.required,
+  ])
+
+  repeatPasswordFormControl = new FormControl('', [
+    Validators.required,
+  ])
+
+  signupForm = new FormGroup({
+    username: this.usernameFormControl,
+    email: this.emailFormControl,
+    password: this.passwordFormControl,
+    repeatPassword: this.repeatPasswordFormControl,
+  })
+
+  toLogin() {
+    this.router.navigate(['/login']).then();
+  }
+}
