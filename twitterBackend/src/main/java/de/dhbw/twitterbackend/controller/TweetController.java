d7697f675d492bb4e20ca0272422057a76136693
JSiggelkow <J.Siggelkow@iks-gmbh.com>
2025-02-20 19:39:33 +0100

    change getAllTweet controller to return oder by time tweets

--- twitterBackend/src/main/java/de/dhbw/twitterbackend/controller/TweetController.java
+++ twitterBackend/src/main/java/de/dhbw/twitterbackend/controller/TweetController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Comparator;
 import java.util.List;
 
 @RestController
@@ -35,6 +36,7 @@ public class TweetController {
 		return ResponseEntity.ok(tweetService.findAll()
 				.stream()
 				.map(tweetMapper::toDTO)
+				.sorted(Comparator.comparing(TweetDTO::createdAt).reversed())
 				.toList()
 		);
 	}
