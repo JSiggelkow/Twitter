32f47726e240488b404fd670db729d929f82e40b
JSiggelkow <J.Siggelkow@iks-gmbh.com>
2025-02-23 12:24:05 +0100

    refactored tweet.component.ts

--- twitterFrontend/src/app/components/shared/tweet/tweet.component.ts
+++ twitterFrontend/src/app/components/shared/tweet/tweet.component.ts
@@ -1,114 +1,81 @@
-import {Component, inject, Input} from '@angular/core';
-import {Avatar} from 'primeng/avatar';
-import {Textarea} from 'primeng/textarea';
-import {FormsModule} from '@angular/forms';
-import {TweetModel} from '../../../model/tweet-model';
-import {TimeAgoComponent} from '../time-ago/time-ago.component';
-import {TweetService} from '../../../service/tweet.service';
-import {NgClass} from '@angular/common';
-import {Popover} from 'primeng/popover';
+import {Component, Input} from '@angular/core';
 import {QuoteModel} from '../../../model/quote-model';
-
+import {TweetModel} from '../../../model/tweet-model';
+import {Avatar} from 'primeng/avatar';
+import {TimeAgoComponent} from '../time-ago/time-ago.component';
+import {Textarea} from 'primeng/textarea';
+import {QuoteComponent} from '../quote/quote.component';
+import {SocialInteractionsComponent} from "../social-interactions/social-interactions.component";
+import {SocialInteractionsModel} from "../../../model/social-interactions-model";
+import {RetweetLabelComponent} from "../retweet-label/retweet-label.component";
 
 @Component({
-  selector: 'app-tweet',
-  imports: [
-    Avatar,
-    Textarea,
-    FormsModule,
-    TimeAgoComponent,
-    NgClass,
-    Popover
-  ],
-  templateUrl: './tweet.component.html',
-  styleUrl: './tweet.component.scss',
-  standalone: true,
+    selector: 'app-tweet',
+    imports: [
+        Avatar,
+        TimeAgoComponent,
+        Textarea,
+        QuoteComponent,
+        SocialInteractionsComponent,
+        RetweetLabelComponent
+    ],
+    templateUrl: './tweet.component.html',
+    styleUrl: './tweet.component.scss'
 })
 export class TweetComponent {
 
-  viewsCount = 12345;
+    @Input() tweet: TweetModel = {
+        id: "",
+        text: "",
+        video: "",
+        image: "",
+        username: "",
+        retweetedByUsername: "",
+        quoteDTO: null,
+        createdAt: "",
+        countLikes: "",
+        countRetweets: "",
+        countComments: "",
+        isLiked: false,
+        isRetweeted: false
+    };
 
-  @Input() quote: QuoteModel | null = null;
-
-  @Input() tweet: TweetModel = {
-    id: "",
-    text: "",
-    video: "",
-    image: "",
-    username: "",
-    retweetedByUsername: "",
-    quoteDTO: null,
-    createdAt: "",
-    countLikes: "",
-    countRetweets: "",
-    countComments: "",
-    isLiked: false,
-    isRetweeted: false
-  };
-
-  tweetService = inject(TweetService);
-
-  get text(): string {
-  return this.quote ? this.quote.text : this.tweet.text;
-  }
-
-  onLike() {
-    if (this.tweet.isLiked) {
-      this.unlike();
-    } else {
-      this.like();
+    get quote(): QuoteModel {
+        return {
+            text: this.tweet.text,
+            createdAt: this.tweet.createdAt,
+            tweetId: this.tweet.id,
+            username: this.tweet.username
+        }
     }
-  }
 
-  like() {
-    this.tweetService.toggleLike(this.tweet.id).subscribe({
-      next: () => {
-        this.tweet.isLiked = true;
-        this.tweet.countLikes = 1 + this.tweet.countLikes;
-      },
-      error: () =>{
-
-      }
-    })
-  }
-
-  unlike() {
-    this.tweetService.toggleLike(this.tweet.id).subscribe({
-      next: () => {
-        this.tweet.isLiked = false;
-        this.tweet.countLikes = -1 + this.tweet.countLikes;
-      },
-      error: () =>{}
-    })
-  }
-
-  onRetweet() {
-    if (this.tweet.isRetweeted) {
-      this.unRetweet();
-    } else {
-      this.retweet()
+    get socialInteractions(): SocialInteractionsModel {
+        return {
+            tweetId: this.tweet.id,
+            quote: {
+                tweetId: this.tweet.id,
+                text: this.tweet.text,
+                createdAt: this.tweet.createdAt,
+                username: this.tweet.username,
+            },
+            viewsCount: 53921, //hardcoded
+            countComments: +this.tweet.countComments,
+            countLikes: +this.tweet.countLikes,
+            countRetweets: +this.tweet.countRetweets,
+            isLiked: this.tweet.isLiked,
+            isRetweeted: this.tweet.isRetweeted
+        }
     }
-  }
 
-  retweet() {
-    this.tweetService.toggleRetweet(this.tweet.id).subscribe({
-      next: () => {
-        this.tweet.isRetweeted = true;
-        this.tweet.countRetweets = 1 + this.tweet.countRetweets;
-      },
-      error: () =>{}
+    onSocialInteractionsChange(updated: SocialInteractionsModel) {
+        this.tweet = {
+            ...this.tweet,
+            isLiked: updated.isLiked,
+            isRetweeted: updated.isRetweeted,
+            countLikes: updated.countLikes.toString(),
+            countRetweets: updated.countRetweets.toString(),
+            countComments: updated.countComments.toString()
+        };
+
     }
-    )
-  }
-
-  unRetweet() {
-    this.tweetService.toggleRetweet(this.tweet.id).subscribe({
-      next: () => {
-        this.tweet.isRetweeted = false;
-        this.tweet.countRetweets = -1 + this.tweet.countRetweets;
-      },
-      error: () =>{}
-    })
-  }
-
 }
