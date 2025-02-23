2407371c8f695d2182f29e2a418d96746b5571fb
JSiggelkow <J.Siggelkow@iks-gmbh.com>
2025-02-23 21:44:50 +0100

    add toggleSave endpoint

--- twitterBackend/src/main/java/de/dhbw/twitterbackend/service/SaveService.java
+++ twitterBackend/src/main/java/de/dhbw/twitterbackend/service/SaveService.java
@@ -1,5 +1,10 @@
 package de.dhbw.twitterbackend.service;
 
+import de.dhbw.twitterbackend.model.Post;
+import de.dhbw.twitterbackend.model.Save;
+import de.dhbw.twitterbackend.model.SaveId;
+import de.dhbw.twitterbackend.model.User;
+import de.dhbw.twitterbackend.repository.SaveRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +13,33 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class SaveService {
 
+	private final SaveRepository saveRepository;
+
+	private void save(Save save) {
+		saveRepository.save(save);
+	}
+
+	public void toggleSave(Post post, User user) {
+		if (isPostSavedByUser(post, user)) {
+			unSavePost(post, user);
+		} else {
+			savePost(post, user);
+		}
+	}
+
+	public void savePost(Post post, User user) {
+		Save save = new Save();
+		save.setId(new SaveId(user.getId(), post.getId()));
+		save.setPost(post);
+		save.setUser(user);
+		this.save(save);
+	}
+
+	public void unSavePost(Post post, User user) {
+		saveRepository.deleteById(new SaveId(user.getId(), post.getId()));
+	}
+
+	public boolean isPostSavedByUser(Post post, User user) {
+		return saveRepository.existsById(new SaveId(user.getId(), post.getId()));
+	}
 }
