0a0f39fc43f586f87fd7bd8e3cb96d9343f0552d
JSiggelkow <J.Siggelkow@iks-gmbh.com>
2025-02-24 12:11:22 +0100

    add info component only for style

--- twitterFrontend/src/app/components/main/info/info.component.ts
+++ twitterFrontend/src/app/components/main/info/info.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import {InputText} from 'primeng/inputtext';
+import {Button} from 'primeng/button';
+import {Avatar} from 'primeng/avatar';
 
 @Component({
   selector: 'app-info',
   imports: [
-    InputText
+    InputText,
+    Button,
+    Avatar
   ],
   templateUrl: './info.component.html',
   styleUrl: './info.component.scss'
